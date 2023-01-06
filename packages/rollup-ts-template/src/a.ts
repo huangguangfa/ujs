914b9130a9bf2618140a0c6cf27423ebd92a7a85
hgf <1454556135@qq.com>
2023-01-06 17:29:18 +0800

    feat(组件): 添加rollup-ts

---
+++ packages/rollup-ts-template/src/a.ts
@@ -0,0 +1,8 @@
+export const arrowCallback = (s: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      let a = s ?? '1'
+      resolve(111 + a)
+    }, 2000)
+  })
+}
---
+++ packages/rollup-ts-template/src/index.ts
@@ -0,0 +1,7 @@
+import { arrowCallback } from './a'
+
+function tsupfn(num: number) {
+  return num
+}
+
+export { arrowCallback, tsupfn }
