7229eba19f09f1fdb5bcf70df4a002e4f1fe8d42
hgf <1454556135@qq.com>
2023-01-07 23:19:53 +0800

    fix(组件): 调整picocolors引入方式

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,7 +1,7 @@
-import picocolors from "picocolors";
-if (!/pnpm/.test(process.env.npm_execpath || "")) {
+const picocolors = require('picocolors')
+if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.log(
     picocolors.red(`此存储库需要使用pnpm作为包管理器，脚本才能正常工作`)
-  );
-  process.exit(1);
+  )
+  process.exit(1)
 }
