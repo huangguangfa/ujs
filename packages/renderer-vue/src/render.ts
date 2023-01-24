ebbd05613e3a1836c675329aed36e7157c6356c4
hgf <1454556135@qq.com>
2023-01-24 17:16:06 +0800

    feat(组件): new

--- packages/renderer-vue/src/render.ts
+++ packages/renderer-vue/src/render.ts
@@ -3,8 +3,8 @@
 // @ts-ignore
 import { createApp } from 'vue'
 // @ts-ignore
-import { createRouter, RouterHistory } from 'vue-router'
-import { createClientRoutes } from './routes'
+// import { createRouter, RouterHistory } from 'vue-router'
+// import { createClientRoutes } from './routes'
 import { IRouteComponents, IRoutesById } from './types'
 
 export function renderClient(opts: {
@@ -13,28 +13,29 @@ export function renderClient(opts: {
   routeComponents: IRouteComponents
   pluginManager: any
   basename?: string
-  history: RouterHistory
-  app:any
+  // history: RouterHistory
+  app: any
 }) {
-  const routes = createClientRoutes({
-    routesById: opts.routes,
-    routeComponents: opts.routeComponents,
-  }) as any
+  // const routes = createClientRoutes({
+  //   routesById: opts.routes,
+  //   routeComponents: opts.routeComponents,
+  // }) as any
   // @ts-ignore
   let rootContainer = opts.App
 
-  const router = createRouter({
-    history: opts.history,
-    strict: true,
-    routes,
-  })
+  // const router = createRouter({
+  //   history: opts.history,
+  //   strict: true,
+  //   routes,
+  // })
+  console.log('---', rootContainer)
   const app = createApp(rootContainer)
 
-  app.use(router)
-  app.mount(opts.rootElement || 'app')
+  // app.use(router)
+  app.mount(opts.rootElement || '#app')
 
   return {
     app,
-    router,
+    // router,
   }
 }
